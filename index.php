5424acdfe98b333a5047d2a0f12d95a1f295052a
Ferref <kovacsferenc026@gmail.com>
2024-10-02 12:51:41 +0200

    Adatbazis tabla insert-ek, php adatbaziskapcsolat demo

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+include 'includes/dbh.inc.php';
+include 'includes/user.inc.php';
+include 'includes/viewuser.inc.php';
 //Oldal változók beállítása
 $ROOT = "./";                       //Az adott fájl relatív elérése a `generate.php`-hoz képest.
 require_once($ROOT."generate.php"); //`generate.php` meghívása
@@ -10,6 +13,9 @@ $testPage->name = "Booknav"; //title attributum értéke
 //Tartalom
 $content = "Ez a főoldal!";
 $container = $testPage->createContainer($content,"Random Kedvcsináló!", "bi-shuffle");
+
+$users = new ViewUser();
+$users -> showAllUsers();
 
 //Oldal megjelenítése
 echo $testPage->genFramedPage($container);
