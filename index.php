ee79ffbfaf2e1c9ce3ef664bcd6d5e56137fdd28
MangoZoltan <mangozoltan@gmail.com>
2024-10-06 13:45:25 +0200

    CSS update

--- index.php
+++ index.php
@@ -22,49 +22,59 @@ $kedvContainer = $homePage->createContainer($kedvContent,"Random Kedvcsináló",
 //Legnépszerűbb könyvek
 $nKonyvekContent = "<div class=\"row\">
     <div class=\"col-12 col-sm-6 col-md-4 col-lg-12_5 p-3\">
-        <div class=\"book-card rounded-10 d-flex flex-column align-items-center text-center shadow c-pointer\">
-            <div class=\"p-2 cover-container\">
-                <img class=\"d-block mx-auto rounded\"src=\"".$ROOT."media/images/nincs-borito.jpg\"/>
+        <a href=\"".$ROOT."pages/book-overview.php?id=0"."\">
+            <div class=\"book-card d-flex flex-column align-items-center text-center c-pointer\">
+                <div class=\"py-2 px-2 px-md-0 px-lg-3 cover-container\">
+                    <img class=\"d-block mx-auto rounded\"src=\"".$ROOT."media/images/nincs-borito.jpg\"/>
+                </div>
+                <p class=\"book-title m-2\">Narnia 2. - Az oroszlán, a boszorkány és a ruhásszekrény - Illusztrált kiadás</p>
+                <p class=\"font-roboto my-gray mb-2\">C. S. Lewis</p>
             </div>
-            <p class=\"book-title m-2\">Narnia 2. - Az oroszlán, a boszorkány és a ruhásszekrény - Illusztrált kiadás</p>
-            <p class=\"font-roboto my-gray mb-2\">C. S. Lewis</p>
-        </div>
+        </a>
     </div>
     <div class=\"col-12 col-sm-6 col-md-4 col-lg-12_5 p-3\">
-        <div class=\"book-card rounded-10 d-flex flex-column align-items-center text-center shadow c-pointer\">
-            <div class=\"p-2 cover-container\">
-                <img class=\"d-block mx-auto rounded\"src=\"https://s01.static.libri.hu/cover/f9/b/4841405_4.jpg\"/>
+        <a href=\"".$ROOT."pages/book-overview.php?id=0"."\">
+            <div class=\"book-card d-flex flex-column align-items-center text-center c-pointer\">
+                <div class=\"py-2 px-2 px-md-0 px-lg-3 cover-container\">
+                    <img class=\"d-block mx-auto rounded\"src=\"https://s01.static.libri.hu/cover/f9/b/4841405_4.jpg\"/>
+                </div>
+                <p class=\"book-title m-2\">Narnia 2. - Az oroszlán, a boszorkány és a ruhásszekrény - Illusztrált kiadás</p>
+                <p class=\"font-roboto my-gray mb-2\">C. S. Lewis</p>
             </div>
-            <p class=\"book-title m-2\">Narnia 2. - Az oroszlán, a boszorkány és a ruhásszekrény - Illusztrált kiadás</p>
-            <p class=\"font-roboto my-gray mb-2\">C. S. Lewis</p>
-        </div>
+        </a>
     </div>
     <div class=\"col-12 col-sm-6 col-md-4 col-lg-12_5 p-3\">
-        <div class=\"book-card rounded-10 d-flex flex-column align-items-center text-center shadow c-pointer\">
-            <div class=\"p-2 cover-container\">
-                <img class=\"d-block mx-auto rounded\"src=\"https://s01.static.libri.hu/cover/f9/b/4841405_4.jpg\"/>
+        <a href=\"".$ROOT."pages/book-overview.php?id=0"."\">
+            <div class=\"book-card d-flex flex-column align-items-center text-center c-pointer\">
+                <div class=\"py-2 px-2 px-md-0 px-lg-3 cover-container\">
+                    <img class=\"d-block mx-auto rounded\"src=\"https://s01.static.libri.hu/cover/f9/b/4841405_4.jpg\"/>
+                </div>
+                <p class=\"book-title m-2\">Narnia 2. - Az oroszlán, a boszorkány és a ruhásszekrény - Illusztrált kiadás</p>
+                <p class=\"font-roboto my-gray mb-2\">C. S. Lewis</p>
             </div>
-            <p class=\"book-title m-2\">Narnia 2. - Az oroszlán, a boszorkány és a ruhásszekrény - Illusztrált kiadás</p>
-            <p class=\"font-roboto my-gray mb-2\">C. S. Lewis</p>
-        </div>
+        </a>
     </div>
     <div class=\"col-12 col-sm-6 col-md-4 col-lg-12_5 p-3\">
-        <div class=\"book-card rounded-10 d-flex flex-column align-items-center text-center shadow c-pointer\">
-            <div class=\"p-2 cover-container\">
-                <img class=\"d-block mx-auto rounded\"src=\"https://s01.static.libri.hu/cover/f9/b/4841405_4.jpg\"/>
+        <a href=\"".$ROOT."pages/book-overview.php?id=0"."\">
+            <div class=\"book-card d-flex flex-column align-items-center text-center c-pointer\">
+                <div class=\"py-2 px-2 px-md-0 px-lg-3 cover-container\">
+                    <img class=\"d-block mx-auto rounded\"src=\"https://s01.static.libri.hu/cover/f9/b/4841405_4.jpg\"/>
+                </div>
+                <p class=\"book-title m-2\">Narnia 2. - Az oroszlán, a boszorkány és a ruhásszekrény - Illusztrált kiadás</p>
+                <p class=\"font-roboto my-gray mb-2\">C. S. Lewis</p>
             </div>
-            <p class=\"book-title m-2\">Narnia 2. - Az oroszlán, a boszorkány és a ruhásszekrény - Illusztrált kiadás</p>
-            <p class=\"font-roboto my-gray mb-2\">C. S. Lewis</p>
-        </div>
+        </a>
     </div>
     <div class=\"col-12 col-sm-6 col-md-4 col-lg-12_5 p-3\">
-        <div class=\"book-card rounded-10 d-flex flex-column align-items-center text-center shadow c-pointer\">
-            <div class=\"p-2 cover-container\">
-                <img class=\"d-block mx-auto rounded\"src=\"https://s01.static.libri.hu/cover/f9/b/4841405_4.jpg\"/>
+        <a href=\"".$ROOT."pages/book-overview.php?id=0"."\">
+            <div class=\"book-card d-flex flex-column align-items-center text-center c-pointer\">
+                <div class=\"py-2 px-2 px-md-0 px-lg-3 cover-container\">
+                    <img class=\"d-block mx-auto rounded\"src=\"https://s01.static.libri.hu/cover/f9/b/4841405_4.jpg\"/>
+                </div>
+                <p class=\"book-title m-2\">Narnia 2. - Az oroszlán, a boszorkány és a ruhásszekrény - Illusztrált kiadás</p>
+                <p class=\"font-roboto my-gray mb-2\">C. S. Lewis</p>
             </div>
-            <p class=\"book-title m-2\">Narnia 2. - Az oroszlán, a boszorkány és a ruhásszekrény - Illusztrált kiadás</p>
-            <p class=\"font-roboto my-gray mb-2\">C. S. Lewis</p>
-        </div>
+        </a>
     </div>
 </div>";
 $nKonyvekContainer = $homePage->createContainer($nKonyvekContent,"Legnépszerűbb könyvek", "bi-book");
