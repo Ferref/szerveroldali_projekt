fdecbb346cc519e8a72ea91d0ec861b3fd52ea9c
MangoZoltan <mangozoltan@gmail.com>
2024-10-20 13:42:42 +0200

    Login and registration Design

--- pages/registration.php
+++ pages/registration.php
@@ -1,43 +1,64 @@
-<!DOCTYPE html>
-<html lang="hu">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Regisztráció</title>
-    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/style.css">
-    <script src="../bootstrap/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
-    <script type="text/javascript" src="../handlers/userdata_empty_handler.js" defer></script>
-</head>
-<body>
-<!-- Using a mixture of bootstrap 5.3.3 and a style.css (custom stylesheet)-->
-    <div class="overlay">
-        <div class="form-container">
-            <form method="post" action="registration_handler.php" id="registration-form" class="w-100 max-w-500px">
-                <h2 class="mb-4">Regisztráció</h2>    
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email cím:</label>
-                    <input type="email" class="form-control" id="email" name="email" required>
-                </div>
-                <div class="mb-3">
-                    <label for="confirm_email" class="form-label">Email cím megerősítése</label>
-                    <input type="email" class="form-control" id="confirm_email" name="confirm_email" required>
-                </div>
-                <div class="mb-3">
-                    <label for="username" class="form-label">Felhasználónév:</label>
-                    <input type="text" class="form-control" id="username" name="username" required>
-                </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">Jelszó:</label>
-                    <input type="password" class="form-control" id="password" name="password" required>
-                </div>
-                <div class="mb-3">
-                    <label for="confirm_password" class="form-label">Jelszó megerősítése:</label>
-                    <input type="password" class="form-control" id="confirm_password" name="confirm_password" required>
-                </div>
-                <button type="submit" class="btn btn-primary">Regisztráció</button>
-            </form>
-        </div>
-    </div>
-</body>
-</html>
+<?php
+//----------------------
+//  Oldal változók beállítása
+//----------------------
+
+$ROOT = "../";                       //Az adott fájl relatív elérése a `generate.php`-hoz képest.
+//$SRC = "https://localhost/szerveroldali_projekt/";
+require_once($ROOT . "generate.php"); //`generate.php` meghívása
+$homePage = new Generate();
+$homePage->root = $ROOT;     //relatív útvonal átadása az osztályban használt elérésekhez (css, képek...)
+$homePage->name = "Regisztráció"; //title attributum értéke
+//echo $ROOT."media/images/nincs-borito.jpg";
+//----------------------
+//      Tartalom
+//----------------------
+
+//Bejelentkezés
+$loginContent = '
+ <div class="container">
+     <div class="row m-0">
+         <div class="col-12 bg-white rounded-20 p-3">
+             <div class="row m-0">
+                 <div class="col-6 py-4 px-5 bg-blue-wave rounded-10">
+                     <form method="post" action="login_handler.php" id="login-form" class="w-100 max-w-500px">
+                        <h2 class="mb-4 text-center text-white">Regisztráció</h2>    
+                        <div class="bg-white rounded py-4 px-5">
+                            <div class="mb-3">
+                                <label for="email" class="form-label my-light-blue">E-mail:</label>
+                                <input type="text" class="form-control" id="email" name="email" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="username" class="form-label my-light-blue">Felhasználónév:</label>
+                                <input type="text" class="form-control" id="username" name="username" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label my-light-blue">Jelszó:</label>
+                                <input type="password" class="form-control" id="password" name="password" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="password2" class="form-label my-light-blue">Jelszó ismét:</label>
+                                <input type="password" class="form-control" id="password2" name="password2" required>
+                            </div>
+                            <button type="submit" class="btn d-block mx-auto shadow bg-my-blue my-white-blue">Regisztrálok</button>
+                        </div>
+                    </form>
+                 </div>
+                 <div class="col-6 py-4 px-5 d-flex flex-column justify-content-center">
+                     
+                        <h2 class="mb-4 text-center my-blue">Már van fiókja?</h2>    
+                        <p class="text-center my-light-blue my-3 ">
+                            Ha már van fiókja csak lépjen be, és máris élvezheti oldalunkat!
+                        </p>
+                        <a href="login.php" class="my-3 btn d-block mx-auto shadow bg-my-white-blue my-blue">Belépek</a>                
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
+';
+
+//---
+
+//Oldal megjelenítése
+echo $homePage->genFramedPage($loginContent);
