e979054208d087fa231f2a6d26c8b7e806a29071
szzvaft <szogi.zsombor@gmail.com>
2024-11-03 09:55:58 +0100

    Delete mvc directory

--- mvc/config/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbConnection = new mysqli('localhost', 'root', '', 'konyvajanlo');
-
-if ($dbConnection->connect_error) {
-    die("Connection failed: " . $dbConnection->connect_error);
-}
-?>
