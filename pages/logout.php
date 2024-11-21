b7deb87343579c459cb5b7f4d8d518bbd2eeb109
szzvaft <szogi.zsombor@gmail.com>
2024-11-21 23:38:44 +0100

    Delete pages directory

--- pages/logout.php
+++
@@ -1,8 +0,0 @@
-<?php session_start();
-  
-$$_SESSION["user_id"]=null;
-$_SESSION["user_name"]=null;
-$_SESSION["user_role"]="guest";
-$_SESSION["user_profilkep"]=null;
-header("Location: ../index.php");
-
