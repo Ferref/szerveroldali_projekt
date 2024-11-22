1eeee09205d99bd739850b410396a907593746ca
szzvaft <szogi.zsombor@gmail.com>
2024-11-22 16:37:41 +0100

    Egyesítés mainnel

--- index.php
+++ index.php
@@ -38,54 +38,18 @@ $kedvContent = $rt_element;
 $kedvContainer = $homePage->createContainer($kedvContent,"Random Kedvcsináló", "bi-shuffle");
 //---
 
+
+$legnepszerubbKonyvek=new BookView();
+$legnepszerubbKonyvek=$legnepszerubbKonyvek->showMostPopularBooks();
+
+
 //Legnépszerűbb könyvek
-$nKonyvekContent = "<div class=\"row\">
-    <div class=\"col-12 col-sm-6 col-md-4 col-lg-12_5 p-3\">
-        <a href=\"".$ROOT."pages/book-overview.php?id=0"."\">
-            <div class=\"book-card rounded border d-flex flex-column align-items-center text-center c-pointer\">
-                <div class=\"py-2 px-2 px-md-0 px-lg-3 cover-container\">
-                    <img class=\"d-block mx-auto rounded\"src=\"".$ROOT."assets/images/nincs-borito.jpg\"/>
-                </div>
-                <p class=\"book-title m-2\">Narnia 2. - Az oroszlán, a boszorkány és a ruhásszekrény - Illusztrált kiadás</p>
-                <p class=\"font-roboto my-gray mb-2\">C. S. Lewis</p>
-            </div>
-        </a>
-    </div>
-    <div class=\"col-12 col-sm-6 col-md-4 col-lg-12_5 p-3\">
-        <a href=\"".$ROOT."pages/book-overview.php?id=0"."\">
-            <div class=\"book-card d-flex flex-column align-items-center text-center c-pointer\">
-                <div class=\"py-2 px-2 px-md-0 px-lg-3 cover-container\">
-                    <img class=\"d-block mx-auto rounded\"src=\"https://s01.static.libri.hu/cover/f9/b/4841405_4.jpg\"/>
-                </div>
-                <p class=\"book-title m-2\">Narnia 2. - Az oroszlán, a boszorkány és a ruhásszekrény - Illusztrált kiadás</p>
-                <p class=\"font-roboto my-gray mb-2\">C. S. Lewis</p>
-            </div>
-        </a>
-    </div>
-    <div class=\"col-12 col-sm-6 col-md-4 col-lg-12_5 p-3\">
-        <a href=\"".$ROOT."pages/book-overview.php?id=0"."\">
-            <div class=\"book-card d-flex flex-column align-items-center text-center c-pointer\">
-                <div class=\"py-2 px-2 px-md-0 px-lg-3 cover-container\">
-                    <img class=\"d-block mx-auto rounded\"src=\"https://s01.static.libri.hu/cover/f9/b/4841405_4.jpg\"/>
-                </div>
-                <p class=\"book-title m-2\">Narnia 2. - Az oroszlán, a boszorkány és a ruhásszekrény - Illusztrált kiadás</p>
-                <p class=\"font-roboto my-gray mb-2\">C. S. Lewis</p>
-            </div>
-        </a>
-    </div>
-    <div class=\"col-12 col-sm-6 col-md-4 col-lg-12_5 p-3\">
-        <a href=\"".$ROOT."pages/book-overview.php?id=0"."\">
-            <div class=\"book-card d-flex flex-column align-items-center text-center c-pointer\">
-                <div class=\"py-2 px-2 px-md-0 px-lg-3 cover-container\">
-                    <img class=\"d-block mx-auto rounded\"src=\"https://s01.static.libri.hu/cover/f9/b/4841405_4.jpg\"/>
-                </div>
-                <p class=\"book-title m-2\">Narnia 2. - Az oroszlán, a boszorkány és a ruhásszekrény - Illusztrált kiadás</p>
-                <p class=\"font-roboto my-gray mb-2\">C. S. Lewis</p>
-            </div>
-        </a>
-    </div>
-    ".book_mini(1,$ROOT)."
-</div>";
+$nKonyvekContent = "<div class=\"row\">";
+foreach($legnepszerubbKonyvek as $konyv) {
+    $nKonyvekContent.=book_mini($konyv,$ROOT);
+}
+    
+$nKonyvekContent.="</div>";
 $nKonyvekContainer = $homePage->createContainer($nKonyvekContent,"Legnépszerűbb könyvek", "bi-book");
 //---
 
